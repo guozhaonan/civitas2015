4e5f23df1038358ab4c0759ad5b50ce54468441a
Albert Guo <guozhaonan@gmail.com>
2015-02-07 13:53:38 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Civitas2015::Application.config.secret_key_base = 'dfc6794953c8b35e01426e6e1772203c8f42f65e91985f5795f911878681fac9e1c2268a29f2bab6d1482ef7b2acd2166cf83f603cd02528fd5e470d3fffdf1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Civitas2015::Application.config.session_store :cookie_store, key: '_civitas2015_session'
