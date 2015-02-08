c6bd402a26724cea0ee865e63ed9bec3388a9028
Albert Guo <guozhaonan@gmail.com>
2015-02-07 18:10:09 -0600

    Starting profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
+    raise :test
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
     redirect_to root_path
