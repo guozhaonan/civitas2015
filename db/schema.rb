200dcb779e3738e23c3a948d8f2aef19a2f301e6
Christina Platt <christinajeanplatt@gmail.com>
2015-02-08 07:59:54 -0500

    fixed styleing on sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -67,4 +66,3 @@ ActiveRecord::Schema.define(version: 20150208121324) do
   end
 
 end
->>>>>>> Stashed changes
